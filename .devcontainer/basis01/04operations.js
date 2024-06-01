9f2bd4a85d1638c6c0357d767398f9fb99e6db28
AnujAuti <anujauti02@gmail.com>
2024-06-01 14:39:39 +0000

    operations

---
+++ .devcontainer/basis01/04operations.js
@@ -0,0 +1,14 @@
+let x = 10
+let y=-x
+//console.log(y); --> -10
+let a ="hi "
+let b =" hello"
+let c = a +b 
+//console.log(c);
+//console.log("1"+ 2); --> 12
+//console.log("1"+ "2"); --> 12
+//console.log(1 + 2 +3+ "3"); --> 63
+let i = 12
+/*i++; --> i=13
+console.log(+true) -->1
+console.log(+false);-->0 */
