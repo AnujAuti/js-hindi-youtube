fe0af59a41078ee7d7212f30bf2181e5acece605
AnujAuti <anujauti02@gmail.com>
2024-06-01 16:06:17 +0000

    comparison

---
+++ .devcontainer/basis01/04comparison.js
@@ -0,0 +1,10 @@
+/* console.log(null>=0);-->true  
+console.log(null==0);--> false
+console.log(null>0);--> false */
+
+/* console.log(undefined>0);--> false 
+console.log(undefined==0);--> false 
+console.log(undefined>=0);--> false */
+ /* console.log("2"==2); == it does not check the datatype 
+console.log("5"===5); === it does  check the datatype  */
+ 
--- .devcontainer/test.js
+++ .devcontainer/test.js
@@ -1,8 +1,10 @@
 console.log("str"); console.log(1 +2)
 //code readability should be good thus dont use the above code
-let x;
+let x="str"
 let y = 123
-z= null
+let z ;
 console.log(typeof x);
 console.log(typeof y);
 console.log(typeof z);
+console.table([typeof x,typeof y]);
+
