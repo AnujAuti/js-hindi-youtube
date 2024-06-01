02d119b7b1cdb01422efbbf8a34c6ea910440257
AnujAuti <anujauti02@gmail.com>
2024-06-01 09:18:08 +0000

    msge1

--- .devcontainer/test.js
+++ .devcontainer/test.js
@@ -1,1 +1,8 @@
-console.log("anuj")
+console.log("str"); console.log(1 +2)
+//code readability should be good thus dont use the above code
+let x;
+let y = 123
+z= null
+console.log(typeof x);
+console.log(typeof y);
+console.log(typeof z);
