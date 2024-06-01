30f62f0f58263e8a3e6742c75afd399967bbd034
AnujAuti <anujauti02@gmail.com>
2024-06-01 13:42:48 +0000

    conversion datatypes

---
+++ .devcontainer/basis01/03conversion opertation.js
@@ -0,0 +1,23 @@
+let x = "22"
+let y =Number(x)
+//console.log(y);
+//console.log(typeof y);
+// use Number() to convert 
+let Booleanval = true
+let Booleanval2 = false
+let boolnum2 = Number(Booleanval2)
+let boolnum = Number(Booleanval)
+//console.log(boolnum);
+//console.log(typeof boolnum);
+//console.log(boolnum2);
+//console.log(typeof boolnum2);
+// true -> 1 ; false -> 0
+let a= undefined
+let b = Number(a)
+/* console.log(typeof b);
+console.log(b);
+undefined --> NaN(Not a number)*/
+let c = ""
+let d = Number(c)
+console.log(d);
+console.log(typeof d);
