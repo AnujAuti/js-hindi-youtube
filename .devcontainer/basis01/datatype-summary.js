7bab762b43c788866d7cccd796bc024ecadcd6bd
AnujAuti <anujauti02@gmail.com>
2024-06-02 11:19:13 +0000

    datatype-summary

---
+++ .devcontainer/basis01/datatype-summary.js
@@ -0,0 +1,12 @@
+const x= Symbol('12')
+const y = Symbol('12')
+//console.log(x==y); --> false
+
+const a = ["hi", "bye", "hello"]
+//console.log(typeof a);
+let obj ={key:"value" , sock :"leg"}
+// console.log(typeof obj); --> object 
+ /* const myfun = function(){
+    console.log("hi");
+}  */
+
