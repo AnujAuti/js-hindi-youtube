accd126802dc4e1cb64b97d681621b5777a54d85
AnujAuti <anujauti02@gmail.com>
2024-06-04 07:19:13 +0000

    arrays

---
+++ .devcontainer/02basics/01_arrays.js
@@ -0,0 +1,16 @@
+const myarr = [ 1,2,3,4]
+//console.log(myarr[3]);
+const myA2 = new Array(10,20,30)
+//console.log(myA2);
+myarr.push(9)
+//console.log(myarr);
+//console.log(myarr.includes(9));
+//console.log(myarr.join())
+//console.log(myarr)
+const x = [1,2,3,4]
+const y =x.slice(0,3) 
+console.log(y);
+console.log(x);
+//const z = x.splice(1,3)
+//console.log(z);
+//console.log(x,y,z);
