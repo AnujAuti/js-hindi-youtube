908c919695fb0574003abffd2734c49f89b948b5
AnujAuti <anujauti02@gmail.com>
2024-06-04 04:33:12 +0000

    math.

---
+++ .devcontainer/basis01/06_num and math.js
@@ -0,0 +1,15 @@
+const x = new Number(100000)
+//console.log(x.toFixed(0));
+// console.log(x.toPrecision(2));
+// console.log(x.toLocaleString('en-In'));
+
+// ----------------------maths------------------------
+//console.log(Math);
+
+//console.log(Math.abs(-9.23));
+//console.log(Math.round(2.5));
+//console.log(//Math.ceil(2));
+min= 10 
+max= 20
+
+console.log(Math.random()*(max - min +1)+ min);
