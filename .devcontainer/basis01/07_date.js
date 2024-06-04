b3676d0c09c424f3958ff0d0294f793e5a9696fd
AnujAuti <anujauti02@gmail.com>
2024-06-04 05:18:43 +0000

    dates

---
+++ .devcontainer/basis01/07_date.js
@@ -0,0 +1,9 @@
+let x = new Date()
+//console.log(x);
+//console.log(x.toLocaleString()); --> MM/DD/YY
+//console.log(x.toDateString()); --> DAY MM DATE YY
+//console.log(x.toLocaleDateString()); --> MM/DD/YY
+let y = Date.now()
+//console.log(y);--> gives time in millisecound
+let z = new Date("06-04-2024")
+console.log(z.getMonth());
