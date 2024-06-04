5c62982d0ab913c0615f2db1b2babaff6058d57d
AnujAuti <anujauti02@gmail.com>
2024-06-04 17:59:06 +0000

    04objects

---
+++ .devcontainer/02basics/04_objects.js
@@ -0,0 +1,15 @@
+const y = new Object()
+const x= {}
+x.name="anuj"
+x.age= "18"
+console.log(x);
+/* z = {name: "anuj", msge: "hi" ,username :{uname: "warlord" , password :"123"}}
+console.log(z.username.password);  */
+
+const A={ 1:"a",2:"b"}
+const B ={ 3:"c", 4:"d"}
+const z = {A , B}
+const p =Object.assign({}, A,B)
+console.log(p);
+const q = {...A,...B}
+console.log(z);
