db3413cf467c4b52c5c9a455492ac480afa747ac
AnujAuti <anujauti02@gmail.com>
2024-06-20 09:20:47 +0000

    loop

---
+++ .devcontainer/04_Controlflow/truthy.js
@@ -0,0 +1,12 @@
+const ary= []
+if(0){console.log('hi');  
+}
+else if(12){
+console.log('hello');
+}
+else{
+    console.log('bye');
+}
+//  '', 0, -0,BigInt 0n , null, undefined , NaN , false 
+
+// '0', '-0','false',' ',12,{}
---
+++ .devcontainer/05 _iterations/one.js
@@ -0,0 +1,16 @@
+ /* for (let x = 1; x<10; x++) {
+//    console.log(x);
+}
+ let myArray = ["flash", "batman", "superman"]
+console.log(myArray.length);
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+console.log(element);}  */
+
+for (let num = 0;num<10 ; num++) {   
+    if (num==5){
+        console.log(` 5 is the best`);
+        continue
+    }
+    console.log(num);
+}
